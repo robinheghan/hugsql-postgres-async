6dc72c9b7095434271132f4e43468d7cf90090a2
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2015-12-02 21:31:00 +0100

    Release 0.3.1

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject skinney/hugsql-adapter-postgres-async "0.3.1-SNAPSHOT"
+(defproject skinney/hugsql-adapter-postgres-async "0.3.1"
   :description "Postgres.async adapter for HugSQL"
   :url "https://github.com/Skinney/hugsql-async"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.374"]
-                 [com.layerware/hugsql-adapter "0.3.0"]
+                 [com.layerware/hugsql-adapter "0.3.1"]
                  [alaisi/postgres.async "0.6.0"]])
